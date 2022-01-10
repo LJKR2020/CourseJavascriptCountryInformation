5be343ff752b2b1069d841e25924df1266a46f9f
LJKR2020 <loredana.rietveld@gmail.com>
2022-01-10 13:31:32 +0100

    made function al countries and made li item on index.html

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log('Hallo daar!');
+import axios from 'axios';
+
+async function allCountries(e) {
+    try {
+        const result = await axios.get("https://restcountries.com/v2/all");
+        console.log(result.data);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+allCountries("");
+
+const countryinfo = document.getElementById("info");
+
+countryinfo.innerHTML = `
+    <h5>vlag landnaam</h5>
+    <p>has a population of ... people</p>
+`;
